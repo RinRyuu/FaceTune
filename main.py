4ad89648b2fee7c8e995b1ee5a507fa2b0599cd5
RinRyuu <97811148+RinRyuu@users.noreply.github.com>
2023-09-26 20:55:55 +0000

    made it sing on emotion

--- main.py
+++ main.py
@@ -1,105 +1,72 @@
 import cv2
 import numpy as np
+import pygame
 from tensorflow.keras.models import load_model
-from collections import Counter
+from collections import deque
 
-# Load the trained emotion recognition model
-emotion_model = load_model('emotion_model.h5')  # Replace with your actual model path
+model = load_model('emotion_model.h5')
 
-# List of emotion labels
 emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
-# Load the Haar Cascade classifier for face detection
-face_cascade = cv2.CascadeClassifier('/home/rin/PycharmProjects/FaceTune/IOT Project/haarcascade-frontalface-default.xml')  # Replace with your actual XML file path
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Initialize variables for emotion tracking
-window_emotions = []  # Emotions detected in the 10-second window
-window_duration = 10  # 10 seconds
-frame_rate = 30  # Assuming 30 frames per second
-show_video = [True]  # Flag to control video display using a mutable list
+pygame.mixer.init()
 
-# Create an OpenCV window with a graphical button
-cv2.namedWindow('Emotion Detection')
-button_text = "Toggle Video On" if show_video[0] else "Toggle Video Off"
+emotion_queue = deque(maxlen=5)
+emotion_songs = {
+    'Angry': 'angry.mp3',
+    'Happy': 'happy.mp3',
+    'Sad': 'sad.mp3',
+}
 
-# Define button colors and fonts
-button_bg_color = (0, 0, 255)  # Red background when video is off
-button_text_color = (255, 255, 255)  # White text color
-font = cv2.FONT_HERSHEY_SIMPLEX
-font_scale = 0.7
-font_thickness = 2
-button_height, button_width = 40, 200
-button_x, button_y = 20, 20
+current_song = None
 
-def toggle_video_display(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        show_video[0] = not show_video[0]
-        update_button_text()
-
-def update_button_text():
-    global button_text
-    button_text = "Toggle Video On" if show_video[0] else "Toggle Video Off"
-
-# Set the mouse callback function to handle button clicks
-cv2.setMouseCallback('Emotion Detection', toggle_video_display)
-
-# Open the camera
-cap = cv2.VideoCapture(0)  # Initialize the video capture object
+cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cap.read()  # Capture a frame from the camera
+    ret, frame = cap.read()
 
-    if show_video[0]:
-        # Detect faces in the frame
-        faces = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    faces = face_cascade.detectMultiScale(frame, 1.3, 5)
 
-        for (x, y, w, h) in faces:
-            face_roi = frame[y:y + h, x:x + w]  # Extract the face region
-            resized = cv2.resize(face_roi, (48, 48))
-            normalized = resized / 255.0
-            reshaped = np.reshape(normalized, (1, 48, 48, 3))
+    detected_emotions = []
 
-            # Predict the emotion using the loaded model
-            emotion_scores = emotion_model.predict(reshaped)
-            emotion_index = np.argmax(emotion_scores)
-            detected_emotion = emotion_labels[emotion_index]
+    for (x, y, w, h) in faces:
+        face_roi = frame[y:y + h, x:x + w]
+        resized = cv2.resize(face_roi, (48, 48))
+        normalized = resized / 255.0
+        reshaped = np.reshape(normalized, (1, 48, 48, 3))
 
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(frame, detected_emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        result = model.predict(reshaped)
+        emotion_index = np.argmax(result)
+        emotion = emotion_labels[emotion_index]
 
-            # Add detected emotion to the window_emotions list
-            window_emotions.append(detected_emotion)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-    # Draw the toggle button
-    if show_video[0]:
-        button_bg_color = (0, 255, 0)  # Green background when video is on
-    else:
-        button_bg_color = (0, 0, 255)  # Red background when video is off
+        detected_emotions.append(emotion)
 
-    cv2.rectangle(frame, (button_x, button_y), (button_x + button_width, button_y + button_height), button_bg_color, -1)
-    cv2.putText(frame, button_text, (button_x + 10, button_y + 30), font, font_scale, button_text_color, font_thickness)
+    emotion_queue.append(detected_emotions)
+
+    if len(emotion_queue) == 5:
+        flat_emotions = [emotion for sublist in emotion_queue for emotion in sublist]
+
+        if flat_emotions:
+            most_common_emotion = max(set(flat_emotions), key=flat_emotions.count)
+
+            if most_common_emotion in emotion_songs:
+                song_filename = emotion_songs[most_common_emotion]
+
+                if current_song != song_filename:
+                    if current_song is not None:
+                        pygame.mixer.music.stop()
+                    current_song = song_filename
+                    pygame.mixer.music.load(song_filename)
+                    pygame.mixer.music.play()
 
     cv2.imshow('Emotion Detection', frame)
 
-    key = cv2.waitKey(1) & 0xFF
-
-    if key == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-    # Calculate the time elapsed
-    elapsed_time = len(window_emotions) / frame_rate
-
-    # Check if the 10-second window has elapsed
-    if elapsed_time >= window_duration:
-        # Count the most frequent emotion in the window
-        emotion_counter = Counter(window_emotions)
-        most_common_emotion = emotion_counter.most_common(1)[0][0]
-
-        # Print the most common emotion
-        print(f"Most Common Emotion in the Last 10 Seconds: {most_common_emotion}")
-
-        # Clear the window_emotions list for the next window
-        window_emotions.clear()
 
 cap.release()
 cv2.destroyAllWindows()
